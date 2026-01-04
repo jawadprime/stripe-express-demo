1eb55dbe88110f7b557bfb821a8b3288892a0802
jawad-prime-dev <jawadahmed.dev@gmail.com>
2026-01-04 22:43:54 +0500

    Finalize payment gateway impl with dummy data

--- apps/api/src/application/payment-gateway.ts
+++ apps/api/src/application/payment-gateway.ts
@@ -1,5 +1,6 @@
-import { Result, DomainEventTuple } from "@stripe-express-demo/shared";
+import { Result, DomainEventTuple, Payment, PaymentConfirmationSecret, IdempotencyKey } from "@stripe-express-demo/shared";
 
 export interface IPaymentGateway{
+    createOneTimePayment(payment: Payment, idempotencyKey: IdempotencyKey): Promise<Result<{ payment: Payment; clientSecret: PaymentConfirmationSecret }>>;
     constructEvent(rawBody: Buffer, signature: string): Promise<Result<DomainEventTuple>>;
 }
--- apps/api/src/infrastructure/mock-stripe-payment-gateway.impl.ts
+++ apps/api/src/infrastructure/mock-stripe-payment-gateway.impl.ts
@@ -1,24 +1,49 @@
-import { Payment, Result, PaymentReference, HasPaymentReference, ok, DomainEvent, DomainEventType, Amount, CustomerId, Currency, PaymentCreatedEvent, DomainEventTuple } from "@stripe-express-demo/shared";
+import { Payment, Result, HasPaymentReference, ok, DomainEventType, Amount, CustomerId, Currency, PaymentCreatedEvent, DomainEventTuple, PaymentConfirmationSecret, StripeEvent, HasCustomerId } from '@stripe-express-demo/shared';
 import { IPaymentGateway } from "../application/payment-gateway";
-import { StripeEventType } from "./stripe-event-type";
-import { HasPaymentId } from "@stripe-express-demo/shared/src/domain/value-objects";
+import { mapStripePaymentStatusToDomain, StripePaymentStatus } from "./stripe-payment-status";
+import { HasPaymentId } from "@stripe-express-demo/shared";
 
-export class MockStripePaymentGateway implements IPaymentGateway{
-  async constructEvent(rawBody: Buffer, signature: string) : Promise<Result<DomainEventTuple>> {
+export class MockStripePaymentGateway implements IPaymentGateway {
+  async constructEvent(
+    rawBody: Buffer,
+    signature: string
+  ): Promise<Result<DomainEventTuple>> {
+    const stripeEvent = StripeEvent.PaymentIntentCreated;
+    let domainEvent: DomainEventTuple;
 
-    console.log("constructing event")
-    const stripeEvent = StripeEventType.PaymentCreated;
-
-    const domainEvent = (() => { switch (stripeEvent) {
-      case StripeEventType.PaymentCreated: {
-        return [
-          new PaymentCreatedEvent(new HasPaymentId("pi_123"), "cus_321" as CustomerId, { amount: 100 as Amount, currency: "usd" as Currency }),
+    switch (stripeEvent) {
+      case StripeEvent.PaymentIntentCreated:
+        domainEvent = [
+          new PaymentCreatedEvent(
+            new HasPaymentId("pi_3MtwBwLkdIwHu7ix28a3tqPa"),
+            new HasCustomerId("cus_NffrFeUfNV2Hib"),
+            { amount: 100 as Amount, currency: "usd" as Currency }
+          ),
           DomainEventType.PaymentCreated
-        ] as const
-      }
+        ] as const;
+        break;
 
-    }})();
+      default:
+        throw new Error(`Unhandled stripe event: ${stripeEvent}`);
+    }
 
-    return ok(domainEvent)
+    return ok(domainEvent);
+  }
+
+  async createOneTimePayment(
+    payment: Payment
+  ): Promise<Result<{ payment: Payment; clientSecret: PaymentConfirmationSecret }>> {
+    return ok({
+      payment: new Payment({
+        ...payment,
+        reference: new HasPaymentReference("pi_3MtwBwLkdIwHu7ix28a3tqPa"),
+        money: {
+          amount: 2000 as Amount,
+          currency: "usd" as Currency
+        },
+        status: mapStripePaymentStatusToDomain(StripePaymentStatus.PaymentRequiresAction)
+      }),
+      clientSecret: "pi_3MtwBwLkdIwHu7ix28a3tqPa_secret_3MtwBwLkdIwHu7ix28a3tqPa" as PaymentConfirmationSecret
+    });
   }
 }
